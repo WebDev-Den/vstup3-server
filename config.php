7ef24fde89a1b8cb61df81fbecd5b4e83e2d7c5c
root <root@vstupsoft.78.152.160.5>
2025-11-29 17:00:03 +0000

    Auto-save: 2025-11-29 17:00:01

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
-define("JWT_KEY", "T2g4j56V8LjPHA2jby3FEAs88p4Sex.X927T5j58rkiiJ7ZTemi64xySK4DAH.4iLRL5455Bydz3x7Prn9h83VLEkkFU");
+define("JWT_KEY", "lU8jeG4tiK0ysI5iW7zbP1mcQ2uzD3l3ifKfdJ9qiG0goU1neZ8sqP7bA6ualX8yyC7ggL1ccM9ruZ5ihA0qe6dzJ4");
 define("JWT_TIME", 3600);
 define("JWT_TIME_REFRESH", 2592000);
 
 
-define("DB_HOST", '192.168.1.48');
-define("DB_USER", 'vstup_2_administrator');
-define("DB_PASSWORD", 'jowIqoYeM5wAC1j57e6uM4yuq1DI4e');
-define("DB_NAME", 'vstup');
+define("DB_HOST", 'localhost');
+define("DB_USER", 'user_vstup');
+define("DB_PASSWORD", 'DEj174iKezid-1n315_tu!N14kIbeJosACa');
+define("DB_NAME", 'db_vstup');
 
 
 $HTTP_KEY_LICENSE = [
-    'E865C447861EAB3DB4441AF9A2BF252E'
+    'bV3mqE4sZ7viS1wnI9zlP2qjL5rpHl'
 ];
 
 
